1cc54c2670967cccf33d20e317ad3d138de0847a
Aviithapa <abhishekthapa115@gmail.com>
2022-03-04 10:55:18 +0545

    added default value

--- database/migrations/2022_02_05_162610_create_site_settings_table.php
+++ database/migrations/2022_02_05_162610_create_site_settings_table.php
@@ -14,7 +14,7 @@ class CreateSiteSettingsTable extends Migration
     public function up()
     {
         Schema::create('site_settings', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->default(1);
             $table->string('display_name')->nullable();
             $table->string('name')->nullable();
             $table->text('value')->nullable()->default(null);
