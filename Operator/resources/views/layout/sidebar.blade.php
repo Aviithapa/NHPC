d7878f6e026eb65c660bb5296c9c15b47ff2f666
Abhishek <49526090+Aviithapa@users.noreply.github.com>
2022-03-31 17:10:23 +0545

    Added sidebar active status in operator

--- Operator/resources/views/layout/sidebar.blade.php
+++ Operator/resources/views/layout/sidebar.blade.php
@@ -17,46 +17,46 @@
         <!-- sidebar menu -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">PERSONAL</li>
-            <li class="active">
+            <li class="{{ (request()->is('operator/dashboard')) ? 'active':''  }}">
                 <a href="{{route('operator.dashboard')}}">
                     <i class="icon-home"></i> <span>Dashboard</span>
 
                 </a>
             </li>
-            <li class="treeview"> <a href="#"> <i class="icon-grid"></i> <span>Applicant Profile</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
+            <li class="treeview {{ (request()->is('operator/dashboard/operator/applicant-profile-list/*/*')) ? 'active':''  }}"> <a href="#"> <i class="icon-grid"></i> <span>Applicant Profile</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
                 <ul class="treeview-menu">
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-profile-list/Reviewing/computer_operator')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.profile.list", ['status'=> 'Reviewing','state' => 'computer_operator'])}}">
                             <i class="icon-book-open"></i> <span>Applicant Profile List  <span class="badge badge-pill badge-success">{{getApplicantCount('Reviewing','computer_operator')}}</span></span>
                         </a>
                     </li>
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-profile-list/Rejected/computer_operator')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.profile.list", ['status'=> 'Rejected', 'state' => 'student'])}}">
                             <i class="icon-ban"></i> <span>Reject Profile List <span class="badge badge-pill badge-danger">{{getApplicantCount('Rejected','student')}}</span></span>
                         </a>
                     </li>
 
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-profile-list/Reviewing/officer')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.profile.list", ['status'=> 'Reviewing', 'state' => 'officer'])}}">
                             <i class="icon-badge"></i> <span>Operator Verified Profile List <span class="badge badge-pill badge-danger">{{getApplicantCount('Reviewing','officer')}}</span></span>
                         </a>
                     </li>
                 </ul>
             </li>
-            <li class="treeview"> <a href="#"> <i class="icon-grid"></i> <span>Exam Applied</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
+            <li class="treeview {{ (request()->is('operator/dashboard/operator/applicant-list/*/*')) ? 'active':''  }}"> <a href="#"> <i class="icon-grid"></i> <span>Exam Applied</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
                 <ul class="treeview-menu">
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-list/progress/computer_operator')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.list", ['status'=> 'progress','state' => 'computer_operator'])}}">
                             <i class="icon-book-open"></i> <span>Exam Applied List <span class="badge badge-pill badge-success">{{getExamApplicantList('progress','computer_operator')}}</span></span>
                         </a>
                     </li>
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-list/progress/officer')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.list",['status'=> 'progress','state' => 'officer'])}}">
                             <i class="icon-book-open"></i> <span>Exam Applied Progress List <span class="badge badge-pill badge-success">{{getExamApplicantList('progress','officer')}}</span></span>
                         </a>
                     </li>
 
-                    <li class="">
+                    <li class="{{ (request()->is('operator/dashboard/operator/applicant-list/rejected/computer_operator')) ? 'active':''  }}">
                         <a href="{{route("operator.applicant.list",['status'=> 'rejected', 'state' => 'computer_operator'])}}">
                             <i class="icon-book-open"></i> <span>Exam Applied Rejected List <span class="badge badge-pill badge-danger">{{getExamApplicantList('rejected','computer_operator')}}</span></span>
                         </a>
@@ -64,7 +64,7 @@
                 </ul>
             </li>
 
-            <li class="">
+            <li class="{{ (request()->is('operator/dashboard/search/student')) ? 'active':''  }}">
                 <a href="{{route("search.student")}}">
                     <i class="icon-book-open"></i> <span>Search applicant</span>
                 </a>
--- SubjectCommittee/routes/subjectCommittee.php
+++ SubjectCommittee/routes/subjectCommittee.php
@@ -1,17 +1,17 @@
 <?php
 
-Route::get('/',  function() {
-    return view('subjectCommittee::pages.dashboard');})->middleware(['auth'])->name('subjectCommittee.dashboard');
+Route::get('/',  function () {
+    return view('subjectCommittee::pages.dashboard');
+})->middleware(['auth'])->name('subjectCommittee.dashboard');
 
 
-Route::get('/subjectCommittee/applicant-list/{status}/{current_state}',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,'exam'])->middleware(['auth'])->name('subjectCommittee.applicant.list');
-Route::get('/subjectCommittee/applicant-profile-list/{status}/{current_state}',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,'profile'])->middleware(['auth'])->name('subjectCommittee.applicant.profile.list');
-Route::get('/subjectCommittee/applicant-list-view/{id}',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,'edit'])->middleware(['auth'])->name('subjectCommittee.applicant.list.review');
-Route::post('/subjectCommittee/applicant-profile-list',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,'status'])->middleware(['auth'])->name('subjectCommittee.applicant.profile.list.status');
-Route::get('/subjectCommittee/verified-applicant-profile-list',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,'verified'])->middleware(['auth'])->name('subjectCommittee.verified.applicant.profile.list');
-Route::get('/subjectCommittee/apply-exam-details/{id}',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,"ExamApplyView"])->middleware(['auth'])->name('subjectCommittee.exam.apply');
-Route::get('/subjectCommittee/accept-exam-applied/{id}',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,"AcceptExamProcessing"])->middleware(['auth'])->name('subjectCommittee.accept.exam.apply');
-Route::post('/subjectCommittee/rejected-exam-applied',[\SubjectCommittee\Http\Controller\SubjectCommitteeController::class,"RejectExamProcessing"])->middleware(['auth'])->name('subjectCommittee.reject.exam.apply');
-Route::get('/search/student',[\SubjectCommittee\Http\Controller\SearchController::class,"index"])->middleware(['auth'])->name('subjectCommittee.search.student');
-Route::get('/search',[\SubjectCommittee\Http\Controller\SearchController::class,"search"])->middleware(['auth'])->name('search');
-
+Route::get('/subjectCommittee/applicant-list/{status}/{current_state}', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, 'exam'])->middleware(['auth'])->name('subjectCommittee.applicant.list');
+Route::get('/subjectCommittee/applicant-profile-list/{status}/{current_state}', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, 'profile'])->middleware(['auth'])->name('subjectCommittee.applicant.profile.list');
+Route::get('/subjectCommittee/applicant-list-view/{id}', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, 'edit'])->middleware(['auth'])->name('subjectCommittee.applicant.list.review');
+Route::post('/subjectCommittee/applicant-profile-list', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, 'status'])->middleware(['auth'])->name('subjectCommittee.applicant.profile.list.status');
+Route::get('/subjectCommittee/verified-applicant-profile-list', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, 'verified'])->middleware(['auth'])->name('subjectCommittee.verified.applicant.profile.list');
+Route::get('/subjectCommittee/apply-exam-details/{id}', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, "ExamApplyView"])->middleware(['auth'])->name('subjectCommittee.exam.apply');
+Route::get('/subjectCommittee/accept-exam-applied/{id}', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, "AcceptExamProcessing"])->middleware(['auth'])->name('subjectCommittee.accept.exam.apply');
+Route::post('/subjectCommittee/rejected-exam-applied', [\SubjectCommittee\Http\Controller\SubjectCommitteeController::class, "RejectExamProcessing"])->middleware(['auth'])->name('subjectCommittee.reject.exam.apply');
+Route::get('/search/student', [\SubjectCommittee\Http\Controller\SearchController::class, "index"])->middleware(['auth'])->name('subjectCommittee.search.student');
+Route::get('/search', [\SubjectCommittee\Http\Controller\SearchController::class, "search"])->middleware(['auth'])->name('search');
