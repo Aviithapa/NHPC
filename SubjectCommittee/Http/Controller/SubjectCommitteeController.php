b850d2a9660c2b5db94a4da8d11d0b142c7802df
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-06-05 10:30:41 +0545

    Changes

--- SubjectCommittee/Http/Controller/SubjectCommitteeController.php
+++ SubjectCommittee/Http/Controller/SubjectCommitteeController.php
@@ -67,7 +67,6 @@ SubjectCommitteeRepository $subjectCommitteeRepository, SubjectCommitteeUserRepo
     public function profile($status, $current_state, $level)
     {
         if (Auth::user()->mainRole()->name === 'subject_committee') {
-            $users = null;
             $subject_Committee_id = $this->subjectCommitteeUserRepository->getAll()->where('user_id','=',Auth::user()->id)->first();
             $subject_committee = $this->subjectCommitteeRepository->getAll()->where('id','=',$subject_Committee_id['subjecr_committee_id'])->first();
             $level = $level ? $level : 1;
@@ -149,9 +148,6 @@ SubjectCommitteeRepository $subjectCommitteeRepository, SubjectCommitteeUserRepo
                         ->get();
                 }
             }
-
-            dd($users);
-            if ($users != null) {
                 foreach ($users as $user) {
                     foreach ($user as $us) {
                         $log = $this->profileLogsRepository->getAll()->where('profile_id', '=', $us['profile_id'])
@@ -164,7 +160,6 @@ SubjectCommitteeRepository $subjectCommitteeRepository, SubjectCommitteeUserRepo
                         }
                     }
                 }
-            }
             return view('subjectCommittee::pages.applicant-profile-list', compact('data','status','current_state'));
         }else{
             return redirect()->route('login');
