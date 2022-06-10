4b3c0841df6447aba29bfdf04a1b35dca1f6dc5c
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-06-10 11:04:53 +0545

    CHnage

--- Operator/resources/views/pages/certificate.blade.php
+++ Operator/resources/views/pages/certificate.blade.php
@@ -140,13 +140,13 @@
         height: 29.7cm;
         font-family: Arial,serif;
         ">
-    <div class="printLayout" style="padding: 3.5rem 2rem;">
+    <div class="printLayout" style="padding: 3rem 2rem;">
    <div class="header" style="text-align: center; font-weight: 500;">
        <span class="p" style="font-size: 18px">Schedule -3 <br>
            (Relating to sub rule (1) of Rule 10)
        </span>
        <h3 style="margin-top: 5px;
-        font-size: 38px;
+        font-size: 40px;
         font-weight: 600;
         line-height: 0.9;">Nepal Health Professional Council <br>
            <span style="font-weight: 600;         font-size: 18px !important;
