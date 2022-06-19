13c9836ba7f415d0b8e273b077b27d18996f7c65
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-06-19 20:22:21 +0545

    Changes

--- officer/resources/views/layout/header.blade.php
+++ officer/resources/views/layout/header.blade.php
@@ -30,67 +30,67 @@
             </div>
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
-                    <!-- Messages -->
-                    <li class="dropdown messages-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-envelope-o"></i>
-                            <div class="notify">
-                                <span class="heartbit"></span> <span class="point"></span>
-                            </div>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="header">You have 4 new messages</li>
-                            <li>
-                                <ul class="menu">
-                                    <li>
-                                        <a href="#">
-                                            <div class="pull-left">
-                                                <img src="{{asset('dist/img/img1.jpg')}}" class="img-circle" alt="User Image" />
-                                                <span class="profile-status online pull-right"></span>
-                                            </div>
-                                            <h4>Notification</h4>
-                                            <p>I've finished it! See you so...</p>
-                                            <p><span class="time">9:30 AM</span></p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="footer"><a href="#">View All Messages</a></li>
-                        </ul>
-                    </li>
-                    <!-- Notifications  -->
-                    <li class="dropdown messages-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-bell-o"></i>
-                            <div class="notify">
-                                <span class="heartbit"></span> <span class="point"></span>
-                            </div>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="header">Notifications</li>
-                            <li>
-                                <ul class="menu">
-                                    <li>
-                                        <a href="#">
-                                            <div class="pull-left icon-circle red">
-                                                <i class="icon-lightbulb"></i>
-                                            </div>
-                                            <h4>{{ Auth::user()->name }}</h4>
-                                            <p>I've finished it! See you so...</p>
-                                            <p><span class="time">9:30 AM</span></p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="footer">
-                                <a href="#">Check all Notifications</a>
-                            </li>
-                        </ul>
-                    </li>
+{{--                    <!-- Messages -->--}}
+{{--                    <li class="dropdown messages-menu">--}}
+{{--                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                            <i class="fa fa-envelope-o"></i>--}}
+{{--                            <div class="notify">--}}
+{{--                                <span class="heartbit"></span> <span class="point"></span>--}}
+{{--                            </div>--}}
+{{--                        </a>--}}
+{{--                        <ul class="dropdown-menu">--}}
+{{--                            <li class="header">You have 4 new messages</li>--}}
+{{--                            <li>--}}
+{{--                                <ul class="menu">--}}
+{{--                                    <li>--}}
+{{--                                        <a href="#">--}}
+{{--                                            <div class="pull-left">--}}
+{{--                                                <img src="{{asset('dist/img/img1.jpg')}}" class="img-circle" alt="User Image" />--}}
+{{--                                                <span class="profile-status online pull-right"></span>--}}
+{{--                                            </div>--}}
+{{--                                            <h4>Notification</h4>--}}
+{{--                                            <p>I've finished it! See you so...</p>--}}
+{{--                                            <p><span class="time">9:30 AM</span></p>--}}
+{{--                                        </a>--}}
+{{--                                    </li>--}}
+{{--                                </ul>--}}
+{{--                            </li>--}}
+{{--                            <li class="footer"><a href="#">View All Messages</a></li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
+{{--                    <!-- Notifications  -->--}}
+{{--                    <li class="dropdown messages-menu">--}}
+{{--                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                            <i class="fa fa-bell-o"></i>--}}
+{{--                            <div class="notify">--}}
+{{--                                <span class="heartbit"></span> <span class="point"></span>--}}
+{{--                            </div>--}}
+{{--                        </a>--}}
+{{--                        <ul class="dropdown-menu">--}}
+{{--                            <li class="header">Notifications</li>--}}
+{{--                            <li>--}}
+{{--                                <ul class="menu">--}}
+{{--                                    <li>--}}
+{{--                                        <a href="#">--}}
+{{--                                            <div class="pull-left icon-circle red">--}}
+{{--                                                <i class="icon-lightbulb"></i>--}}
+{{--                                            </div>--}}
+{{--                                            <h4>{{ Auth::user()->name }}</h4>--}}
+{{--                                            <p>I've finished it! See you so...</p>--}}
+{{--                                            <p><span class="time">9:30 AM</span></p>--}}
+{{--                                        </a>--}}
+{{--                                    </li>--}}
+{{--                                </ul>--}}
+{{--                            </li>--}}
+{{--                            <li class="footer">--}}
+{{--                                <a href="#">Check all Notifications</a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </li>--}}
                     <!-- User Account  -->
                     <li class="dropdown user user-menu p-ph-res">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="{{asset('dist/img/img1.jpg')}}" class="user-image" alt="User Image" />
+{{--                            <img src="{{asset('dist/img/img1.jpg')}}" class="user-image" alt="User Image" />--}}
                             <span class="hidden-xs">{{ Auth::user()->name }}</span>
                         </a>
                         <ul class="dropdown-menu">
