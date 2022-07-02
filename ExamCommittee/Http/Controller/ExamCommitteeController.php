59deff2a382e16dc2fc640cc51f4cdc76afa9a07
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-07-02 17:40:21 +0545

    changes

--- ExamCommittee/Http/Controller/ExamCommitteeController.php
+++ ExamCommittee/Http/Controller/ExamCommitteeController.php
@@ -99,7 +99,7 @@ class  ExamCommitteeController extends BaseController
             $darta_number = $index['darta_number'];
             foreach ($users as $user) {
                 $index = $i++;
-                $symbol_number = 22070334581 + $index;
+                $symbol_number = 22070219012 + $index;
                 $darta = ++$darta_number;
                 $data['profile_id'] = $user['profile_id'];
                 $data['exam_processing_id'] = $user['id'];
