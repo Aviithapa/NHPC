0e2a638acbfe5e1f0454b128774f1a90f936903d
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-07-12 22:28:37 +0545

    changes

--- Operator/Http/Controller/OperatorController.php
+++ Operator/Http/Controller/OperatorController.php
@@ -592,8 +592,7 @@ class OperatorController extends BaseController
             ->join('provinces','provinces.id','=','profiles.development_region')
             ->join('registrant_qualification','registrant_qualification.user_id','=','profiles.user_id')
             ->where('certificate_history.id','=',$certificate_id)
-
-            ->where('registrant_qualification.level','=','2')
+            ->where('registrant_qualification.level','=','5')
             ->orderBy('certificate_history.id','ASC')
             ->get(['certificate_history.*','certificate_history.name as certificate_name','certificate_history.program_name as certificate_program_name','profiles.*','program.name as Name_program','registrant_qualification.*','provinces.province_name','certificate_history.id as certificate_history_id'])->first();
 
