a2ab3e66c5cba8fd9b96b6a5b3088cb0979ae53f
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-07-14 15:36:22 +0545

    changes

--- Operator/resources/views/pages/certificate.blade.php
+++ Operator/resources/views/pages/certificate.blade.php
@@ -288,7 +288,7 @@
             <td style=" border: 1px solid black;
         text-align: center;  font-size: 16px;
         font-weight: bold;
-        width: 180px;
+        width: 160px;
 
 ">{{$certificate->certificate_program_name}}</td>
             <td style=" border: 1px solid black;
