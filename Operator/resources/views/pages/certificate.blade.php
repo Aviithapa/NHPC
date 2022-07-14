87c6a67bc7c0f223c718fa41d503edbc61676ecb
abhishekthapa115 <abhishekthapa115@gmail.com>
2022-07-14 16:02:51 +0545

    chnages'

--- Operator/resources/views/pages/certificate.blade.php
+++ Operator/resources/views/pages/certificate.blade.php
@@ -288,7 +288,7 @@
             <td style=" border: 1px solid black;
         text-align: center;  font-size: 16px;
         font-weight: bold;
-        width: 145px;
+        width: 140px;
 
 ">{{$certificate->certificate_program_name}}</td>
             <td style=" border: 1px solid black;
