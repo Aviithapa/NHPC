6ee9e6eca8dd60ce3a92b69a8b20dca107c54eec
Abhishek Thapa <abhishekthapa115@gmail.com>
2022-10-12 21:35:38 +0545

    cahnages

--- Council/Http/Controller/CouncilController.php
+++ Council/Http/Controller/CouncilController.php
@@ -234,12 +234,12 @@ class CouncilController extends BaseController
                 //            ->where('exam_registration.isPassed',"=",true)
                 ->where('exam_registration.certificate_generate', '=', 'No')
                 ->orderBy('profiles.created_at', 'ASC')
-                ->skip(0)
-                ->take(500)
                 ->get([
                     'profiles.*', 'profiles.id as profile_id', 'profiles.created_at as profile_created_at', 'program.name as program_name', 'program.*',
                     'program.id as program_id', 'level.*', 'provinces.province_name', 'exam_registration.id as exam_registration_id'
                 ]);
+
+            dd($students);
             foreach ($students as $student) {
                 $srn_number = 0;
                 $date = '2022-09-21';
