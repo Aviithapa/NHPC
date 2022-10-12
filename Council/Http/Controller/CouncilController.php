2d733250a62d098d4fdaa54bd111fb502c466563
Abhishek Thapa <abhishekthapa115@gmail.com>
2022-10-12 13:47:16 +0545

    chnages

--- Council/Http/Controller/CouncilController.php
+++ Council/Http/Controller/CouncilController.php
@@ -119,13 +119,13 @@ class CouncilController extends BaseController
     {
         if (Auth::user()->mainRole()->name === 'council') {
             $data = $request->all();
-            $date = isset($date);
-            $certificate = '';
-            if (isset($date)) {
+            $dates = isset($data['date']);
+            $date = isset($data['date']) ? $data['date'] : '';
+            if ($dates) {
                 $certificate = DB::table('certificate_history')
                     ->select('program_id', 'level_name', 'program_certificate_code',  DB::raw('count(*) as total'), DB::raw('group_concat(srn) as srns'))
                     ->groupBy('program_id', 'level_name', 'program_certificate_code')
-                    ->orWhere('decision_date', '=', $date)
+                    ->orWhere('decision_date', '=', $data['date'])
                     ->orderBy('level_name')
                     ->orderBy('srn', 'ASC')
                     ->get(array('srn'))
@@ -139,19 +139,18 @@ class CouncilController extends BaseController
                     ->get(array('srn'))
                     ->unique('program_id');
             }
-            $certificate = DB::table('certificate_history')
-                    ->select('program_id', 'level_name', 'program_certificate_code',  DB::raw('count(*) as total'), DB::raw('group_concat(srn) as srns'))
-                    ->groupBy('program_id', 'level_name', 'program_certificate_code')
-                    ->orderBy('level_name')
-                    ->orderBy('srn', 'ASC')
-                    ->get(array('srn'))
-                    ->unique('program_id');
+            // $certificate = DB::table('certificate_history')
+            //         ->select('program_id', 'level_name', 'program_certificate_code',  DB::raw('count(*) as total'), DB::raw('group_concat(srn) as srns'))
+            //         ->groupBy('program_id', 'level_name', 'program_certificate_code')
+            //         ->orderBy('level_name')
+            //         ->orderBy('srn', 'ASC')
+            //         ->get(array('srn'))
+            //         ->unique('program_id');
             $selectedDate = isset($date) ? $date : '';
             $data = isset($date) ? DB::table('certificate_history')
                 ->where('decision_date', '=', $date)
                 ->count() : 0;
 
-                dd($certificate);
             return \view('council::pages.darta-book', compact('certificate', 'date', 'selectedDate', 'data'));
         } else {
             return redirect()->route('login');
