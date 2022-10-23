bc0308025fa281232090622d1c71b0b61dae8064
Abhishek Thapa <abhishekthapa115@gmail.com>
2022-10-23 07:47:27 +0545

    changes

--- Operator/Http/Controller/OperatorController.php
+++ Operator/Http/Controller/OperatorController.php
@@ -1190,7 +1190,7 @@ class OperatorController extends BaseController
         $tasks =  Certificate::join('admit_card', 'admit_card.profile_id', 'certificate_history.profile_id')
             ->where('certificate_history.level_name', '=', 'Second')
             ->where('certificate_history.decision_date', '=', '2022-09-21')
-            ->whereDate('admit_card.created_at','!=',date('2022-07-01'))
+            ->where('admit_card.created_at','!=',date('2022-07-01'))
             ->get();
 
         $headers = array(
