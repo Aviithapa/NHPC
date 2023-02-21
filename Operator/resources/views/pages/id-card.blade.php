8867eb56b51166804ab03f054eb08820b01b9d55
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-02-21 21:11:27 +0545

    id card print

--- Operator/resources/views/pages/id-card.blade.php
+++ Operator/resources/views/pages/id-card.blade.php
@@ -47,10 +47,10 @@
                     <span>Signature       : ....................................................</span></br>
                    
                 </div>
-                <div class="logo" style="width: 20%;float:left; text-align:center; font-size:11px; margin-top:-15px;">
-                    <span style="z-index: 2000;"> <img src="https://nhpc.gov.np/beta//assets/img/nhpc_logo.jpg" class="logo" width="25px" height="25px" style="margin-bottom: -30px; z-index: 10000; margin-left:45px;"></span></br>
-                    <img class="img-responsive" width="90" height="90" style="margin-top: 20px;" src={{ $profile->getProfileImage() }}></br>
-                    <span style="z-index: 2000; "> <img src="http://103.175.192.52/storage/documents/tKtyJYTTE9JiesiME3bLts5EMNGQgL3IQV6BF5Qp.png" class="signature" width="50px" height="25px" style="margin-top: -30px; z-index: 10000;"></span></br>
+                <div class="logo" style="width: 20%;float:left; text-align:center; font-size:11px;">
+                    <span style="z-index: 2000;"> <img src="https://nhpc.gov.np/beta//assets/img/nhpc_logo.jpg" class="logo" width="25px" height="25px" style="margin-bottom: -15px; margin-top:10px; z-index: 10000; margin-left:45px;"></span></br>
+                    <img class="img-responsive" width="90" height="90" src={{ $profile->getProfileImage() }}></br>
+                    <span style="z-index: 2000; "> <img src="http://103.175.192.52/storage/documents/tKtyJYTTE9JiesiME3bLts5EMNGQgL3IQV6BF5Qp.png" class="signature" width="50px" height="25px" style="margin-top: -10px; z-index: 10000;"></span></br>
                     <span>Registrar</span>
                 </div>
               </div>
