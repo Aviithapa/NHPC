84ba756d260b5ea6f9100c489d0621aff2a8fc70
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-03-21 12:52:18 +0545

    Changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,6 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-       
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
