68b0a61097a0fef3a7b393cc8d9de25cabd685e3
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-05-06 22:11:13 +0545

    cahnges

--- SubjectCommittee/Http/Controller/SubjectCommitteeController.php
+++ SubjectCommittee/Http/Controller/SubjectCommitteeController.php
@@ -611,14 +611,13 @@ class SubjectCommitteeController extends BaseController
             ->join('program', 'program.id', '=', 'exam_registration.program_id')
             ->join('profile_processing', 'profile_processing.profile_id', '=', 'profiles.id')
             ->where('profile_processing.current_state', 'subject_committee')
-            ->where('exam_registration.level_id', '<', '3')
+            ->where('exam_registration.exam_id', '=', '5')
             ->where('program.subject-committee_id', $subject_Committee['subjecr_committee_id'])
             ->where('profile_processing.status', 'progress')
             // ->where('profile_processing.subject_committee_accepted_num', '>', 2)
             ->orderBy('profiles.created_at', 'ASC')
             ->get(['profiles.*']);
 
-        dd($datas);
         $data = $this->subjectCommitteeUserRepository->getAll()->where('user_id', '=', Auth::user()->id)->first();
         $subject_committee = $this->subjectCommitteeRepository->findById($data['subjecr_committee_id']);
         return view('subjectCommittee::pages.exam', compact('datas', 'subject_committee'));
@@ -660,7 +659,7 @@ class SubjectCommitteeController extends BaseController
             ->join('profile_processing', 'profile_processing.profile_id', '=', 'profiles.id')
             ->where('profile_processing.current_state', 'subject_committee')
             ->where('profile_processing.status', 'progress')
-            ->where('exam_registration.level_id', '<', '4')
+            ->where('exam_registration.exam_id', '=', '5')
             ->where('program.subject-committee_id', $subject_Committee['subjecr_committee_id'])
             // ->where('profile_processing.subject_committee_accepted_num', '>=', 2)
             ->orderBy('profiles.created_at', 'ASC')
