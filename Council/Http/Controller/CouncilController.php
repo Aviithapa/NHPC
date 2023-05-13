61e7d374541d543ca5b20e865704969f0a4f8b33
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-05-13 23:10:01 +0545

    changes

--- Council/Http/Controller/CouncilController.php
+++ Council/Http/Controller/CouncilController.php
@@ -186,8 +186,8 @@ class CouncilController extends BaseController
             // }
             // $certificates = ExamProcessing::where('status', '!=', 'accepted')->get();
 
-            $certificates = $this->certificateRepository->getAll()->where('decision_date', '=', '2023-04-21')->where('program_id', '=', '51');
-            $srn = 68;
+            $certificates = $this->certificateRepository->getAll()->where('decision_date', '=', '2023-04-21')->where('program_id', '=', '12');
+            $srn = 16;
             foreach ($certificates as $certificate) {
                 $data['srn'] = $srn++;
                 $data['cert_registration_number'] =  $data['srn']  . ' OPT';
