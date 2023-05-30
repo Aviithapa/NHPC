c98ad4f006fd7a5ad5ad966c0f2d47291f587774
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-05-30 12:05:36 +0545

    changes

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,25 +6,32 @@ namespace App\Http\Controllers;
 
 use App\Mail\ProfileStatus;
 use App\Mail\SignupEmail;
+use Exception;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public static function sendSignupEmail($name, $email, $verification_code){
-        $data = [
-            'name' => $name,
-            'verification_code' => $verification_code
-        ];
-        Mail::to($email)->send(new SignupEmail($data));
-
+    public static function sendSignupEmail($name, $email, $verification_code)
+    {
+        try {
+            $data = [
+                'name' => $name,
+                'verification_code' => $verification_code
+            ];
+            Mail::to($email)->send(new SignupEmail($data));
+        } catch (Exception $e) {
+        }
     }
 
-    public static function sendprofileVerification($name, $email, $verification_code){
-        $data = [
-            'name' => $name,
-            'verification_code' => $verification_code
-        ];
-        Mail::to($email)->send(new ProfileStatus($data));
-
+    public static function sendprofileVerification($name, $email, $verification_code)
+    {
+        try {
+            $data = [
+                'name' => $name,
+                'verification_code' => $verification_code
+            ];
+            Mail::to($email)->send(new ProfileStatus($data));
+        } catch (Exception $e) {
+        }
     }
 }
