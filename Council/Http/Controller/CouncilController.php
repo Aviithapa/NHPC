4ba5f312465382b4af5febe341d5f98165ef1dda
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-06-03 21:44:33 +0545

    RD

--- Council/Http/Controller/CouncilController.php
+++ Council/Http/Controller/CouncilController.php
@@ -189,11 +189,11 @@ class CouncilController extends BaseController
             // $certificates = ExamProcessing::where('status', '!=', 'accepted')->get();
 
             $certificates = $this->certificateRepository->getAll()->where('decision_date', '=', '2023-06-02')
-                ->where('program_id', '=', '34');
-            $srn =  10227;
+                ->where('program_id', '=', '47');
+            $srn =  2601;
             foreach ($certificates as $certificate) {
                 $data['srn'] = $srn++;
-                $data['cert_registration_number'] =  'B-' . $data['srn']  . ' MLT';
+                $data['cert_registration_number'] =  'B-' . $data['srn']  . ' RD';
                 // $data['decision_date'] = '2023-06-02';
                 // $data['issued_date'] = '2023-06-02';
                 $updatedDecisionDate = $this->certificateRepository->update($data, $certificate['id']);
