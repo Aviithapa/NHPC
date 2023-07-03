81e92298575ce1563bc1ea408d013ad72e6018ec
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-07-03 10:49:53 +0545

    chnages

--- Operator/resources/views/pages/certificate/print.blade.php
+++ Operator/resources/views/pages/certificate/print.blade.php
@@ -210,7 +210,7 @@
         Province is registered as <span style="font-size: 26px;
         font-weight: 700;">
            {{-- {!! htmlspecialchars_decode($certificate->certificate_program_name) !!} --}}
-           {{ $data->program_code}}
+           {{ $data->code_}}
         </span>
         of <span style="font-size: 26px;
         font-weight: 700;">{{$data->level}}</span> Level
