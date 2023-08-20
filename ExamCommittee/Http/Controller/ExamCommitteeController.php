11675eda6d8d4673a8bccfe3055f4fcbd55f26c6
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-08-20 15:11:33 +0545

    Changes

--- ExamCommittee/Http/Controller/ExamCommitteeController.php
+++ ExamCommittee/Http/Controller/ExamCommitteeController.php
@@ -115,7 +115,7 @@ class  ExamCommitteeController extends BaseController
                 session()->flash('success', 'Admit Card Already Been Generated');
                 return redirect()->back()->withInput();
             } else {
-                $i = 547;
+                $i = 1133;
                 $index = ExamProcessing::orderBy('darta_number', 'desc')->first();
                 $darta_number = $index['darta_number'];
                 foreach ($users as $user) {
