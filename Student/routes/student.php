5608b21dad2d6a0ee3fdc8900c2b798289b22e87
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-08-20 16:10:20 +0545

    Changes

--- Student/routes/student.php
+++ Student/routes/student.php
@@ -35,7 +35,7 @@ Route::get('/link/certificate', [\Student\Http\Controller\CertificateController:
 Route::get('/link/certificate/form', [\Student\Http\Controller\CertificateController::class, 'edit'])->middleware(['auth'])->name('certificate.edit');
 Route::post('/link/certificate/validateCertificate', [\Student\Http\Controller\CertificateController::class, 'validateCertificate'])->middleware(['auth'])->name('certificate.validateCertificate');
 
-Route::get('/admit/card/print/index', [\Student\Http\Controller\ProfileController::class, 'admitCardPrintSection'])->middleware(['auth'])->name('admit.card.admitCardPrintSection');
+// Route::get('/admit/card/print/index', [\Student\Http\Controller\ProfileController::class, 'admitCardPrintSection'])->middleware(['auth'])->name('admit.card.admitCardPrintSection');
 Route::post('/admit/card/print', [\Student\Http\Controller\ProfileController::class, 'admitCardRequestTemplate'])->middleware(['auth'])->name('admit.card.admitCardRequestTemplate');
 Route::get('/admit/admitCardProfileId/{id}', [\Student\Http\Controller\ProfileController::class, 'admitCardProfileId'])->middleware(['auth'])->name('admit.card.admitCardProfileId');
 
