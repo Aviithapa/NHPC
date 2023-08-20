d88ca240ff870cc9a8b853071e04f4c71f3e8fc9
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-08-20 13:25:20 +0545

    Changes

--- ExamCommittee/Http/Controller/ExamCommitteeController.php
+++ ExamCommittee/Http/Controller/ExamCommitteeController.php
@@ -698,7 +698,7 @@ class  ExamCommitteeController extends BaseController
     public function getAllStudentList()
     {
 
-        $exams = ExamProcessing::all()->where('state', '=', 'exam_committee')->where('status', '=', 'progress')->where('is_admit_card_generate', '=', 'yes')->where('exam_id', '=', 6);
+        $exams = ExamProcessing::all()->where('state', '=', 'subject_committee')->where('status', '=', 'progress')->where('is_admit_card_generate', '=', 'yes')->where('exam_id', '=', 6);
         dd($exams);
         foreach ($exams as $exam) {
             $data['exam_id'] = 5;
