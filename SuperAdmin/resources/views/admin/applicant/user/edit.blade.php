ca7c827caebea67091f18df1df35cd13f3ef4e7f
Abhishek Thapa <abhishekthapa115@gmail.com>
2023-12-10 13:52:44 +0545

    Name edit

--- SuperAdmin/resources/views/admin/applicant/user/edit.blade.php
+++ SuperAdmin/resources/views/admin/applicant/user/edit.blade.php
@@ -30,6 +30,12 @@
                                         <div class="row">
                                             <div class="col-lg-4">
                                                 <fieldset class="form-group">
+                                                    <label>Name </label>
+                                                    <input name="name" class="form-control" id="basicInput" type="text" value="{{ $data->name }}" required/>
+                                                </fieldset>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <fieldset class="form-group">
                                                     <label>Email </label>
                                                     <input name="email" class="form-control" id="basicInput" type="text" value="{{ $data->email }}" required/>
                                                 </fieldset>
