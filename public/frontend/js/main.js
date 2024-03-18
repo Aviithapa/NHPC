c9d760437c526293ee01c62d512c52abb099c6e7
Abhishek Thapa <abhishekthapa115@gmail.com>
2024-03-18 11:39:27 +0545

    remove wow init

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,69 +1,71 @@
-$('#owl1').owlCarousel({
+$("#owl1").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
     autoplay: true,
     responsive: {
         0: {
-            items: 1
-        }
-    }
+            items: 1,
+        },
+    },
 });
 
-$('#owl3').owlCarousel({
+$("#owl3").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
-    autoplayHoverPause:false,
+    autoplayHoverPause: false,
     autoplay: true,
     responsive: {
         0: {
-            items: 1
-        }
-    }
+            items: 1,
+        },
+    },
 });
 
-
-
-$('#owl2').owlCarousel({
+$("#owl2").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
-    autoplayHoverPause:true,
+    autoplayHoverPause: true,
     autoplay: true,
     responsive: {
         0: {
-            items: 1
+            items: 1,
         },
         600: {
-            items: 3
+            items: 3,
         },
         1000: {
-            items: 3
-        }
-    }
+            items: 3,
+        },
+    },
 });
 
 $(window).scroll(function () {
-    var scroll=$(window).scrollTop();
-if(scroll >= 50) {
-$("header").addClass("sticky");
-} else {
-$("header").removeClass("sticky");
-}
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $("header").addClass("sticky");
+    } else {
+        $("header").removeClass("sticky");
+    }
 });
 
-new WOW().init();
-
+// new WOW().init();
 
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
@@ -74,6 +76,6 @@ function scrollFunction() {
 function topFunction() {
     // document.body.scrollTop = 0;
     // document.documentElement.scrollTop = 0;
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
 }
